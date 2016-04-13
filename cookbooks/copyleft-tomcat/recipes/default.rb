b146a4d6cfbba772f49bd0a785b2ad423ba7b111
Brian Hooper <brian.hooper@edointeractive.com>
2016-04-13 15:57:35 -0500

    Added support for Tomcat Users to provision access to admin and manager interfaces

--- cookbooks/copyleft-tomcat/recipes/default.rb
+++ cookbooks/copyleft-tomcat/recipes/default.rb
@@ -13,6 +13,7 @@ include_recipe 'copyleft-java'
 # RECIPE VARIABLES
 tomcat_bin_dir = File.join(node['tomcat']['directory'], "apache-tomcat-#{node['tomcat']['version']}", 'bin')
 
+# CREATED BY COPYLEFT-BASE BY DEFAULT
 # # CREATE THE DIRECTORY
 # directory node['tomcat']['directory'] do
 #   owner node['tomcat']['user']
@@ -21,7 +22,7 @@ tomcat_bin_dir = File.join(node['tomcat']['directory'], "apache-tomcat-#{node['t
 #   action :create
 # end
 
-# directory node['tomcat']['download_directory'] do
+# directory node['tomcat']['deploy_directory'] do
 #   owner node['tomcat']['user']
 #   group node['tomcat']['group']
 #   mode 00755
@@ -53,12 +54,15 @@ end
 
 execute 'unzip_tomcat_zip' do
   user node['tomcat']['user']
+  group node['tomcat']['group']
   command "unzip #{node['tomcat']['deploy_directory']}/apache-tomcat-#{node['tomcat']['version']}.zip -d #{node['tomcat']['directory']}"
   creates "#{node['tomcat']['directory']}/apache-tomcat-#{node['tomcat']['version']}"
+  #notifies :run, 'execute[set_permissions_apache_tomcat_directory]', :immediately
 end
 
 link '/opt/zeus/tomcat' do
   to File.join(node['tomcat']['directory'], "apache-tomcat-#{node['tomcat']['version']}")
+  owner 'root'
 end
 
 %w( startup.sh killtomcat.sh).each do |bin_file|
@@ -67,5 +71,31 @@ end
     owner node['tomcat']['user']
     group node['tomcat']['group']
     source "#{bin_file}.erb"
+  end
+end
+
+# CREATE ADMIN-GUI USER
+template "#{node.base.directory}/tomcat/conf/tomcat-users.xml" do
+  source 'tomcat-users.xml.erb'
+  owner node['tomcat']['user']
+  group node['tomcat']['group']
+  mode '0755'
+end
+
+# execute 'set_permissions_apache_tomcat_directory' do
+#   user node['tomcat']['user']
+#   command <<-EOH
+#
+#     chmod +x #{node{['tomcat']['user']}}:#{node{['tomcat']['group']}} #{node['tomcat']['directory']}/apache-tomcat-#{node['tomcat']['version']}/bin/*.sh
+#     chmod +x #{node{['tomcat']['user']}}:#{node{['tomcat']['group']}} #{node['tomcat']['directory']}/apache-tomcat-#{node['tomcat']['version']}/bin/*.jar
+#   EOH
+#   action :nothing
+# end
+
+%w( shutdown.sh catalina.sh daemon.sh setclasspath.sh tool-wrapper.sh version.sh ).each do |executable_file|
+  file File.join(tomcat_bin_dir, executable_file) do
+    mode 00755
+    owner node['tomcat']['user']
+    group node['tomcat']['group']
   end
 end
