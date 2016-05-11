5b95297b20fb623adea4756f79436e42979f4d92
Thad Scalf <thad.scalf@edointeractive.com>
2016-05-11 14:39:37 -0500

    Add the beats dashboard to Kibana

--- cookbooks/copyleft-elkstack/recipes/kibana.rb
+++ cookbooks/copyleft-elkstack/recipes/kibana.rb
@@ -5,6 +5,7 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 package 'kibana'
+package 'unzip'
 
 template 'kibana.yml' do
   path '/opt/kibana/config/kibana.yml'
@@ -13,4 +14,30 @@ end
 
 service 'kibana' do
   action [:enable, :start]
+end
+
+remote_file 'beats-dashboards-1.1.0.zip' do
+  path File.join(Chef::Config['file_cache_path'], 'beats-dashboards-1.1.0.zip')
+  source 'https://download.elastic.co/beats/dashboards/beats-dashboards-1.1.0.zip'
+  notifies :run, 'execute[install_dashboard]', :immediate
+end
+remote_file 'filebeat-index-template.json' do
+  path File.join(Chef::Config['file_cache_path'], 'filebeat-index-template.json')
+  source 'https://gist.githubusercontent.com/thisismitch/3429023e8438cc25b86c/raw/d8c479e2a1adcea8b1fe86570e42abab0f10f364/filebeat-index-template.json'
+  notifies :run, 'execute[install_beats]', :immediate
+end
+
+execute 'install_dashboard' do
+  action :nothing
+  command "
+    cd #{Chef::Config['file_cache_path']}
+    unzip beats-dashboards-1.1.0.zip
+    cd beats*
+    ./load.sh
+  "
+end
+
+execute 'install_beats' do
+  action :nothing
+  command "curl -XPUT 'http://localhost:9200/_template/filebeat?pretty' -d@#{Chef::Config['file_cache_path']}/filebeat-index-template.json"
 end
--- cookbooks/copyleft-elkstack/spec/unit/recipes/kibana_spec.rb
+++ cookbooks/copyleft-elkstack/spec/unit/recipes/kibana_spec.rb
@@ -18,6 +18,7 @@ describe 'copyleft-elkstack::kibana' do
     end
     it 'installs packages for kibana' do
       expect(chef_run).to install_package('kibana')
+      expect(chef_run).to install_package('unzip')
     end
     it 'creates logstash templates' do
       expect(chef_run).to create_template('kibana.yml')
@@ -25,6 +26,15 @@ describe 'copyleft-elkstack::kibana' do
     it 'starts kibana' do
       expect(chef_run).to enable_service('kibana')
       expect(chef_run).to start_service('kibana')
+    end
+    it 'downloads kibana templates' do
+      expect(chef_run).to create_remote_file('beats-dashboards-1.1.0.zip')
+      expect(chef_run).to create_remote_file('filebeat-index-template.json')
+    end
+
+    it 'does not install kibana templates' do
+      expect(chef_run).to_not run_execute('install_dashboard')
+      expect(chef_run).to_not run_execute('install_beats')
     end
   end
 end
